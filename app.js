76c0cf359eb2e3e3909d72e344f41c599ee8841c
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-08-10 22:02:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.get("/contact", function(req, res){
 
 
 
-
-app.listen(3000 || process.env.PORT, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
 });
