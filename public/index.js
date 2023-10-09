4439b7c1e65c09ce33112fcb98dce0b8593a2c45
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-10-09 12:01:12 +0530

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -13,14 +13,50 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   };
 });
+
+
 //for changing light to dark and vice-versa
 var icon=document.getElementById("icon");
+var icon1=document.getElementById("icon1");
 var webbg1=document.querySelector(".Top_container.row");
 var iconPath=document.getElementById("icon-path");
 var webbgblank=document.querySelectorAll(".bgwhite");
 var butt=document.getElementsByClassName("dlcv");
 var bord=document.getElementsByClassName("infoimgabout");
-icon.onclick=function(){
+icon.onclick=()=>{
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    iconPath.setAttribute("d","M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z")
+    webbg1.style.background="url('css/webbackground3dark.jpg') no-repeat";
+    webbgblank.forEach(function (element) {
+      element.style.backgroundImage = "url('css/blackbg.jpg')";
+    });
+
+    for (var i = 0; i < butt.length; i++) {
+      butt[i].classList.remove("btn-light");
+      butt[i].classList.toggle("btn-dark");
+    }
+    for(var i =0;i<bord.length;i++){
+      bord[i].classList.remove("border-danger");
+      bord[i].classList.toggle("border-white");
+    }
+  }else{
+    iconPath.setAttribute("d","M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z");
+    webbg1.style.background="url('css/webbackground3.jpg') no-repeat";
+    webbgblank.forEach(function (element) {
+      element.style.backgroundImage = "url('css/whitebg.jpg')";
+    });
+    for (var i = 0; i < butt.length; i++) {
+      butt[i].classList.toggle("btn-light");
+      butt[i].classList.remove("btn-dark");
+    }
+    for(var i =0;i<bord.length;i++){
+      bord[i].classList.remove("border-white");
+      bord[i].classList.toggle("border-danger");
+    }
+  }
+}
+icon1.onclick=()=>{
   document.body.classList.toggle("dark-theme");
   if(document.body.classList.contains("dark-theme")){
     iconPath.setAttribute("d","M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z")
@@ -121,4 +157,5 @@ window.addEventListener('resize', updateBackground);
 }
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
 icon.addEventListener("click",updateBackground);
+icon1.addEventListener("click",updateBackground);
 
